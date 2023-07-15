a84e01af30c8d0a0aebf9b1e8462b2bab487427f
tanveer-hash <tanveerhash7@gmail.com>
2023-07-15 14:39:11 +0530

    Unique Atribute assigned to name

--- account/models.py
+++ account/models.py
@@ -39,7 +39,7 @@ class User(AbstractBaseUser):
       max_length=255,
       unique=True,
   )
-  name = models.CharField(max_length=200)
+  name = models.CharField(max_length=200,unique=True)
   is_active = models.BooleanField(default=True)
   is_admin = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
